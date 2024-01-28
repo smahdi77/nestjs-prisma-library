0f13c7c4d752efeafa04266fc6d611dbc1e9ef70
smahdi77 <s.m.mahdi1377@gmail.com>
2024-01-28 20:36:06 +0330

    init prisma && migrate

---
+++ prisma/migrations/20240117073730_init/migration.sql
@@ -0,0 +1,81 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'USER', 'PUBLISHER', 'STORE');
+
+-- CreateEnum
+CREATE TYPE "LogType" AS ENUM ('LOGIN', 'LOGOUT', 'INSERT', 'UPDATE', 'DELETE');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "firs_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "age" INTEGER,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Book" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "writer" TEXT,
+    "translator" TEXT,
+    "price" INTEGER NOT NULL,
+    "publisher" TEXT,
+    "ISBN" TEXT,
+    "editor" TEXT,
+    "topicId" INTEGER NOT NULL,
+
+    CONSTRAINT "Book_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "BookTopic" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "sub_title" TEXT NOT NULL,
+
+    CONSTRAINT "BookTopic_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "BooksOfUsers" (
+    "userId" INTEGER NOT NULL,
+    "bookId" INTEGER NOT NULL,
+    "assignedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "BooksOfUsers_pkey" PRIMARY KEY ("userId","bookId")
+);
+
+-- CreateTable
+CREATE TABLE "Log" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "logType" "LogType" NOT NULL,
+    "timestamp" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Log_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "Book" ADD CONSTRAINT "Book_topicId_fkey" FOREIGN KEY ("topicId") REFERENCES "BookTopic"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BooksOfUsers" ADD CONSTRAINT "BooksOfUsers_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BooksOfUsers" ADD CONSTRAINT "BooksOfUsers_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "Book"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Log" ADD CONSTRAINT "Log_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
