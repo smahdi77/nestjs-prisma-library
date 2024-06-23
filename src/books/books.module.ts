6a3b8588a0af637521b07486c4fd0c61b7514290
a.akbari <Gplus@INTERNET-135>
2024-06-23 14:25:22 +0330

    init book model

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
+import { PrismaService } from 'prisma-database/prisma.service';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
   controllers: [BooksController],
-  providers: [BooksService],
+  providers: [BooksService, PrismaService, UsersService],
 })
 export class BooksModule {}
--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,1 +1,10 @@
-export class CreateBookDto {}
+export class CreateBookDto {
+  title: string;
+  writer: string;
+  translator?: string;
+  price: number;
+  publisher?: string;
+  ISBN?: string;
+  editor?: string;
+  topicId: number;
+}
