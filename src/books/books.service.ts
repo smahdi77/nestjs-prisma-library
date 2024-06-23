6ba600ac1c1589710b0091633f6f05b64eb3d860
a.akbari <Gplus@INTERNET-135>
2024-06-23 14:25:22 +0330

    init book model

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,19 +1,74 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { PrismaService } from 'prisma-database/prisma.service';
+import { UsersService } from 'src/users/users.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class BooksService {
-  create(createBookDto: CreateBookDto) {
-    return 'This action adds a new book';
+  constructor(
+    private prisma: PrismaService,
+    private userService: UsersService,
+  ) {}
+
+  async create(data: CreateBookDto, user_id: number) {
+    const {
+      title,
+      writer,
+      translator,
+      price,
+      publisher,
+      ISBN,
+      editor,
+      topicId,
+    } = data;
+    const new_book = await this.prisma.book.create({
+      data: {
+        title,
+        writer,
+        translator,
+        price,
+        publisher,
+        ISBN,
+        editor,
+        topic: {
+          connect: {
+            id: topicId,
+          },
+        },
+      },
+    });
+    return this.prisma.booksOfUsers.create({
+      data: {
+        userId: user_id,
+        bookId: new_book.id,
+      },
+    });
+  }
+
+  async createTopic(data: Prisma.BookTopicCreateInput) {
+    return this.prisma.bookTopic.create({ data });
   }
 
   findAll() {
-    return `This action returns all books`;
+    return this.prisma.book.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} book`;
+    return this.prisma.book.findUnique({ where: { id } });
+  }
+
+  async findMyBooks(id: number) {
+    const user_books = await this.userService.findOne(id).books();
+    let mybooks = {};
+    await Promise.all(
+      user_books.map(async (book) => {
+        let mybook = await this.findOne(book.bookId);
+        mybooks[book.bookId] = mybook;
+      }),
+    );
+    return mybooks;
   }
 
   update(id: number, updateBookDto: UpdateBookDto) {
